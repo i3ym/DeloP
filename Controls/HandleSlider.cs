be4b7bb4606e0ca80b95aac2bd0814b5f248107b
i3ym <deniso99@mail.ru>
2020-09-23 15:06:19 +0700

    Remove unnesessary using

--- Controls/HandleSlider.cs
+++ Controls/HandleSlider.cs
@@ -3,7 +3,6 @@ using osu.Framework.Graphics;
 using osu.Framework.Graphics.Shapes;
 using osu.Framework.Graphics.Sprites;
 using osu.Framework.Graphics.UserInterface;
-using osu.Framework.Layout;
 
 namespace DeloP.Controls
 {
