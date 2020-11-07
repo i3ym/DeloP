3a8d685517a379487d7cf818c1aba28ba8b88c62
i3ym <deniso99@mail.ru>
2020-11-07 22:10:58 +0700

    Improve menu

---
+++ Controls/DeloMenu.cs
@@ -0,0 +1,44 @@
+using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Effects;
+using osu.Framework.Graphics.Shapes;
+using osu.Framework.Graphics.Sprites;
+using osu.Framework.Graphics.UserInterface;
+
+namespace DeloP.Controls
+{
+    public class DeloMenu : BasicMenu
+    {
+        public DeloMenu(Direction direction, bool topLevelMenu = false) : base(direction, topLevelMenu) { }
+
+        [BackgroundDependencyLoader]
+        void Load() => BackgroundColour = Colors.Background.Darken(.1f);
+
+
+        protected override DrawableMenuItem CreateDrawableMenuItem(MenuItem item) => new DeloMenuItem(item);
+        protected override Menu CreateSubMenu() => new DeloMenu(Direction.Vertical) { Anchor = Direction == Direction.Horizontal ? Anchor.BottomLeft : Anchor.TopRight };
+
+
+        public class DeloMenuItem : DrawableMenuItem
+        {
+            public DeloMenuItem(MenuItem item) : base(item) { }
+
+            [BackgroundDependencyLoader]
+            void Load()
+            {
+                BackgroundColour = Colors.Background;
+                BackgroundColourHover = Colors.DarkBackground;
+            }
+
+            protected override Drawable CreateContent() => new SpriteText()
+            {
+                Anchor = Anchor.CentreLeft,
+                Origin = Anchor.CentreLeft,
+                Padding = new MarginPadding(2),
+                Font = FontUsage.Default.With(family: "Ubuntu"),
+                Text = Item.Text.Value,
+            };
+        }
+    }
+}
--- MainScreen.cs
+++ MainScreen.cs
@@ -1,11 +1,15 @@
 using System.Threading.Tasks;
 using DeloP.Containers;
 using DeloP.Controls;
+using osu.Framework;
 using osu.Framework.Allocation;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Shapes;
+using osu.Framework.Graphics.UserInterface;
 using osu.Framework.Input.Events;
 using osu.Framework.IO.Stores;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Advanced;
 
 namespace DeloP
 {
@@ -13,6 +17,7 @@ namespace DeloP
     {
         const int ToolPanelWidth = 68, ToolSettingsHeight = 32, MenuHeight = 24;
 
+        [Resolved] Game Game { get; set; } = null!;
         readonly FullCanvas FullCanvas = new FullCanvas() { Y = ToolSettingsHeight + 2, X = ToolPanelWidth + 2 };
 
         [BackgroundDependencyLoader]
@@ -27,10 +32,40 @@ namespace DeloP
             Children = new Drawable[]
             {
                 new Box() { RelativeSizeAxes = Axes.Both, Colour = Colors.DarkBackground },
-                new Menu(FullCanvas.Canvas) { RelativeSizeAxes = Axes.X, Height = MenuHeight, Depth = -2 },
+                new DeloMenu(Direction.Horizontal, true) { RelativeSizeAxes = Axes.X, Height = MenuHeight, Depth = -2, Items = CreateMenuItems() },
                 new ToolSettingsPanel(FullCanvas.Canvas) { RelativeSizeAxes = Axes.X, Y = MenuHeight, Height = ToolSettingsHeight, Depth = -1 },
                 new ToolPanel(FullCanvas.Canvas) { RelativeSizeAxes = Axes.Y, Y = ToolSettingsHeight + MenuHeight, Width = ToolPanelWidth, Depth = -1 },
                 FullCanvas,
+            };
+        }
+
+        MenuItem[] CreateMenuItems()
+        {
+            void clickNew()
+            {
+                FullCanvas.Canvas.Image.GetPixelSpan().Fill(Color.White);
+                FullCanvas.Canvas.UpdateImage();
+            }
+
+
+            return new[]
+            {
+                new MenuItem("Файл", () => { })
+                {
+                    Items = new[]
+                    {
+                        new MenuItem("Новый", clickNew),
+                        new MenuItem("Коровый", () => System.Console.WriteLine("чё смотриш пёс")),
+                        new MenuItem("Выйти", Game.Exit),
+                    }
+                },
+                new MenuItem("Нефайл", () => { })
+                {
+                    Items = new[]
+                    {
+                        new MenuItem("хуй", () => {}),
+                    }
+                }
             };
         }
 
